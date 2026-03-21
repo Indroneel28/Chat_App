be4c26354442cc0c38fbd31626098ab65b0893b9
Indroneel Goswami <indroneelgoswami2461@gmail.com>
2026-03-21 21:32:11 +0530

    fix: dbname of mongo collection

--- backend/database/db.js
+++ backend/database/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 export const dbConnection = () => {
     mongoose
     .connect(process.env.MONGO_URI, {
-            dbName: 'CHAT_APPCATION'
+            dbName: 'CHAT_APPLICATION'
         })
         .then(() => {
             console.log('Connected to Mongo DB database');
